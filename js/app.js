15594ba277773a476e87af33d07448516f8dcf26
Tushar Kharat M1 <tushar@Tushars-MacBook-Pro-M1.local>
2021-06-30 15:01:39 +0530

    Content bar added and remove on scroll

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+window.onscroll = function () {
+    myFunction()
+};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("fixed");
+        header.classList.add("top-0");
+    } else {
+        //header.classList.remove("fixed top-0");
+        header.classList.remove("fixed");
+        header.classList.remove("top-0");
+    }
+}
+
 function toggleMenu() {
     var mobile_menu_container = document.getElementById("mobile-menu-container");
-    
+
     if (mobile_menu_container.classList.contains('hidden')) {
         mobile_menu_container.classList.remove('hidden');
         animateCSS('#mobile-menu-container', 'fadeInRight');
